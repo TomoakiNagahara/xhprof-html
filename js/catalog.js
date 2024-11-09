6f1a88e389110ab14e239a39e75592ecc93cfaa5
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-11-10 03:22:34 +0900

    Add: js > catalog.js | GetCatalog()

--- js/catalog.js
+++ js/catalog.js
@@ -7,3 +7,24 @@
  * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright  Tomoaki Nagahara All right reserved.
  */
+
+/** Get catalog json object
+ *
+ * @created    2024-11-09
+ * @version    1.0
+ * @package    xhprof-html
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+function GetCatalog(){
+	//	static value
+	if( typeof GetCatalog.json === 'undefined'　){
+		let text = $("#catalog").text();
+		let json = JSON.parse(text);
+		GetCatalog.json = json;
+		$("#catalog").text('');
+	}
+	//	...
+	return GetCatalog.json;
+}
+
